9bc7091d1dca1f8f5ddd45174c5ce1f00b2bcc0b
Jean Santos <jeansaantos89@gmail.com>
2025-03-05 20:53:16 -0300

    versao-05-03

--- dags/coleta_cripto.py
+++ dags/coleta_cripto.py
@@ -7,53 +7,55 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 from scripts.conexao_mysql import conectar_mysql
 import logging
 
-logging.basicConfig(
+# Definição de log base e formato
+logging.basicConfig( 
     filename = "log_execucao.txt",
     level = logging.INFO,
-    format="%(asctime)s - %(levelname)s - %(message)s",  # Formato do log
+    format="%(asctime)s - %(levelname)s - %(message)s",  
 )
 
-# Extração - Buscar dados da API CoinGecko
+# 1) Extração - Buscar dados da API (CoinGecko)
 def coletar_dados():
     logging.info("Inicializando coleta de CRIPTO DATA")
     
     url = "https://api.coingecko.com/api/v3/coins/markets"
     params = {
         "vs_currency": "usd",  # Converte os preços para dólares
-        "order": "market_cap_desc",  # Ordena por valor de mercado (maiores primeiro)
-        "per_page": 15,  # Pegamos apenas as 5 principais criptomoedas
-        "page": 1,  # Primeira página de resultados
+        "order": "market_cap_desc",  # Ordena por ordem crescente de valor de mercado
+        "per_page": 15,  # Apenas as 15 primeiras moedads
+        "page": 1,  # Apenas a primeira página de resultados
         "sparkline": False  # Não inclui gráficos de tendência
     }
 
+    # Enviar requisição para a API com os parâmetros (params)
     try:
-        response = requests.get(url, params=params) # Envia a requisição HTTP para a API com os parâmetros definidos.
-        response.raise_for_status() 
-        data = response.json() # Converte a resposta JSON para um dicionário
+        response = requests.get(url, params=params) # Envia a requisição HTTP para a API com os parâmetros definidos
+        response.raise_for_status() # Checagem se ocorreu algum erro
+        data = response.json() # Converte JSON para dicionário Python
         logging.info("Coleta de CRIPTO BEM-sucedida.")
     except requests.exceptions.RequestException as e:
-        logging.error("Coleta de CRIPTO MAL-sucedida. ERROR = {e}")
+        logging.error(f"Coleta de CRIPTO MAL-sucedida. ERROR = {e}") # {e} = erro resultante
         return  # Encerra a função sem continuar
 
 
-    cripto_df = pd.DataFrame(data, columns=["symbol", "name", "current_price", "market_cap"]) #cria um dataframe com os nomes que tem os cabeçalhos das coins na api
+    cripto_df = pd.DataFrame(data, columns=["symbol", "name", "current_price", "market_cap"]) # Cria dataframe com os cabeçalhos da API
 
     cripto_df.rename(columns={
         "symbol": "simbolo",
         "name": "nome",
         "current_price": "preco",
         "market_cap": "valor_mercado"
-    }, inplace=True) #renomeia todas para as do MySQL,  inplace=True) == alterar o original
+    }, inplace=True) # Renomeia todas para a nomenclatura do MySQL,  (inplace=True == alterar o original)
 
-    cripto_df["ultima_atualizacao"] = datetime.now()  # Adiciona a data e hora da atualização
+    cripto_df["ultima_atualizacao"] = datetime.now()  # Adiciona a data e hora da última atualização
 
     # Conectar ao MySQL e inserir os dados
     conn = conectar_mysql()
     if conn:
             try:
                 cursor = conn.cursor()
-                cursor.execute("TRUNCATE TABLE cripto_precos;")
-                for _, row in cripto_df.iterrows():
+                cursor.execute("TRUNCATE TABLE cripto_precos;") # Limpa os dados dos preços passados
+                for _, row in cripto_df.iterrows(): #iteração sobre o dataframe
                     cursor.execute("""
                         INSERT INTO cripto_precos (simbolo, nome, preco, valor_mercado, ultima_atualizacao)
                         VALUES (%s, %s, %s, %s, %s)
@@ -61,7 +63,7 @@ def coletar_dados():
                 conn.commit()
                 logging.info("Dados inseridos com sucesso.")
             except Exception as err:
-                logging.info("Falha ao inserir dados: {err}.")
+                logging.info(f"Falha ao inserir dados: {err}.")
             finally:
                 cursor.close()
                 conn.close()
